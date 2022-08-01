076ab9ae126d0cac788e1fd0185387a71c6d19f1
Hadagalberto Junior <hadagalberto.junior1@gmail.com>
2022-08-01 00:26:33 -0300

    bug

--- RaspberryPi.Console/Program.cs
+++ RaspberryPi.Console/Program.cs
@@ -32,10 +32,6 @@ while (!Console.KeyAvailable)
 
     var metrics = client.ReadCpuUsage();
 
-    Console.WriteLine("Core1: " + metrics.Core1Usage);
-    Console.WriteLine("Core2: " + metrics.Core2Usage);
-    Console.WriteLine("Core3: " + metrics.Core3Usage);
-    Console.WriteLine("Core4: " + metrics.Core4Usage);
     Console.WriteLine("All Usage: " + metrics.CpuUsage);
 
     Thread.Sleep(1000);
