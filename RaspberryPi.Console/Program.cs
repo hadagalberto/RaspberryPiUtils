ec3cc07aeb470e1b78d0015288309de104ff1c37
Hadagalberto Junior <hadagalberto.junior1@gmail.com>
2022-08-01 00:06:33 -0300

    Add project files.

---
+++ RaspberryPi.Console/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using Iot.Device.CpuTemperature;
+using Iot.Device.HardwareMonitor;
+
+using CpuTemperature cpuTemperature = new();
+using OpenHardwareMonitor openHardwareMonitor = new();
+Console.WriteLine("Press any key to quit");
+
+while (!Console.KeyAvailable)
+{
+    if (cpuTemperature.IsAvailable)
+    {
+        var temperature = cpuTemperature.ReadTemperatures();
+        foreach (var entry in temperature)
+        {
+            if (!double.IsNaN(entry.Temperature.DegreesCelsius))
+            {
+                Console.WriteLine($"Temperature from {entry.Sensor}: {entry.Temperature.DegreesCelsius} °C");
+            }
+            else
+            {
+                Console.WriteLine("Unable to read Temperature.");
+            }
+        }
+    }
+    else
+    {
+        Console.WriteLine($"CPU temperature is not available");
+    }
+
+    Console.WriteLine(openHardwareMonitor.GetCpuLoad().Percent);
+
+    Thread.Sleep(1000);
+}
